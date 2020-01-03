f59a2449b9e6f2ca6965354d7af8f73861cebb0e
Paweł Szmergała <pawelszme@gmail.com>
2020-01-03 14:38:34 +0100

    Added site crawling rules i.e. you can specify attributes (like id, name, class, title) of 'a' tag

--- main.py
+++ main.py
@@ -3,10 +3,15 @@ import threading
 import re
 import queue
 import bs4
+import time
+import pickle
 
 class CrawlResult:
-    def __init__(self, startAddress, results):
+    def __init__(self, startAddress, maxDepth, startTime, endTime, results):
         self.startAddress = startAddress
+        self.maxDepth = maxDepth
+        self.startTime = startTime
+        self.endTime = endTime
         self.results = results
 
 def downloadSite(toVisit: queue.Queue, downloaded: queue.Queue, l: threading.Lock):
@@ -26,17 +31,17 @@ def searchForSentencesContainingWord(word, tagsListToSearch):
         regEx = re.compile(r'(\b' + word + r'\b|[A-Z][^\.]*?\b' + word + r'\b).*?[\.!?](?:\s|$)')
         bs = bs4.BeautifulSoup(siteHTML, 'html.parser')
         sentencesContainingWord = []
-        for tag in bs.body.findAll():
+        for tag in bs.body.findAll(tagsListToSearch):
             sentencesContainingWord.extend([sentence.group() for sentence in regEx.finditer(tag.text)])
         return sentencesContainingWord
     return aux
 
-def processSite(toVisit: queue.Queue, downloaded: queue.Queue, visited: set, actionRes: list, maxDepth, action, l: threading.Lock):
+def processSite(toVisit: queue.Queue, downloaded: queue.Queue, visited: set, actionRes: list, maxDepth, aAttrsFilter: dict, action, l: threading.Lock):
     while downloaded.empty() == False:
         siteAddress, dist, siteHTML = downloaded.get()
         visited.add(siteAddress)
         bs = bs4.BeautifulSoup(siteHTML, 'html.parser')
-        for tag in bs.body.findAll('a'):
+        for tag in bs.body.findAll('a', attrs=aAttrsFilter):
             if re.match(r'.+\..+\..+', tag.get('href')) and (not tag.get('href') in visited):
                 if dist < maxDepth:
                     toVisit.put((tag.get('href'), dist+1))
@@ -45,12 +50,13 @@ def processSite(toVisit: queue.Queue, downloaded: queue.Queue, visited: set, act
         if len(result) != 0:
             actionRes.append((siteAddress, result))
 
-def crawl(startPage, maxDepth, action):
+def crawl(startPage, maxDepth, aAttrsFilter, action):
     toVisit = queue.Queue()
     downloaded = queue.Queue()
     visited = set()
     actionRes = []
     l = threading.Lock()
+    startTime = time.time()
 
     toVisit.put((startPage, 0))
     while toVisit.empty() == False:
@@ -66,15 +72,15 @@ def crawl(startPage, maxDepth, action):
 
         # processing sites
         for i in range(min(downloaded.qsize(), 8)):
-            t = threading.Thread(target=processSite, args=(toVisit, downloaded, visited, actionRes, maxDepth, action, l))
+            t = threading.Thread(target=processSite, args=(toVisit, downloaded, visited, actionRes, maxDepth, aAttrsFilter, action, l))
             t.start()
             threads.append(t)
         for t in threads:
             t.join()
         threads.clear()
     
-    return CrawlResult(startPage, actionRes)
+    return CrawlResult(startPage, maxDepth, startTime, time.time(), actionRes)
 
-crawlResult = crawl('https://www.python.org/', 1, searchForSentencesContainingWord('Python', []))
+crawlResult = crawl('https://www.python.org/', 1, {}, searchForSentencesContainingWord('Python', []))
 for e in crawlResult.results:
     print(e)
